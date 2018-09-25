1f497ada79d7ec726070aadca7b12700c864064e
AliakseiK19 <positive_on@yahoo.com>
2018-09-25 14:00:22 +0300

    -

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  
+  for (let row = 0; row < bracketsConfig.length; row++) {
+      for (let col = 0; col < bracketsConfig[row].length; col++) {
+          if ((str[row] == bracketsConfig[row][col] || str[row] == bracketsConfig[row + 1][col]) && str[str.length - 1] == bracketsConfig[row][bracketsConfig[col].length - 1] || str[str.length - 1] == bracketsConfig[row+1][bracketsConfig[col].length - 1]) {
+            
+            return true;
+          }
+          else { return false; }
+      }
+  }  
 }
+
+
