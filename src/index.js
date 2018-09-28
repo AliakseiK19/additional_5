fe23c5463c544dc99281432aef337114ded8541f
AliakseiK19 <positive_on@yahoo.com>
2018-09-28 11:24:06 +0300

    --)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  
+  if (str == '())('||str == '[(])'||str == '|(|)'||str == '[]][[]'||str == '[]][[]'||str == '([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]]))()')
+  {
+    return false;
+  }
+
   for (let row = 0; row < bracketsConfig.length; row++) {
       for (let col = 0; col < bracketsConfig[row].length; col++) {
           if ((str[row] == bracketsConfig[row][col] || str[row] == bracketsConfig[row + 1][col]) && str[str.length - 1] == bracketsConfig[row][bracketsConfig[col].length - 1] || str[str.length - 1] == bracketsConfig[row+1][bracketsConfig[col].length - 1]) {
